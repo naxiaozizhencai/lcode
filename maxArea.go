fe751ebb387767337f5125ce9b93e1e9fb60c22d
chaozi <1243352372@qq.com>
2019-09-05 15:27:22 +0800

    no message

--- maxArea.go
+++ maxArea.go
@@ -2,11 +2,36 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("aaa")
+func main() {
+	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
 }
 
 func maxArea(height []int) int {
-	return 0
-}
+	if len(height) < 2 {
+		return 0
+	}
 
+	l := 0
+	r := len(height) - 1
+
+	var area int
+	var temp_area int
+
+	for l < r {
+
+		if height[l] > height[r] {
+			temp_area = height[r] * (r - l)
+			r--
+		} else {
+			temp_area = height[l] * (r - l)
+			l++
+		}
+		fmt.Println(temp_area)
+		if temp_area > area {
+			area = temp_area
+		}
+
+	}
+
+	return area
+}
