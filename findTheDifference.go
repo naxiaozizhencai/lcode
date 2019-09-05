51657d22e178c8638b0dfd79a1dd19da20b0a332
chaozi <1243352372@qq.com>
2019-09-05 14:12:18 +0800

    no message

---
+++ findTheDifference.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(findTheDifference("abcd", "abcde"))
+}
+
+func findTheDifference(s string, t string) byte{
+	var result uint8
+
+	for _,v := range []rune(s)  {
+		result -= uint8(v)
+	}
+
+	for _,v := range []rune(t)  {
+		result += uint8(v)
+	}
+
+	return result
+}
