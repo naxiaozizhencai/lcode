cf7c886760ed481ca0cd12e6d10916ac6faa3598
chaozi <1243352372@qq.com>
2019-09-05 13:50:19 +0800

    no message

--- singleNumber.go
+++ singleNumber.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("aaa")
+	fmt.Println(singleNumber([]int{4,1,2,1,2}))
+}
+
+func singleNumber(nums []int) int {
+	result := 0
+	for i:=0 ;i<len(nums);i++{
+		result ^= nums[i]
+	}
+
+	return result
 }
