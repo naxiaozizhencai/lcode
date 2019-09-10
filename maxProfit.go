2bfaaf9453efe64f723812a24b81d5b5ff88fb4b
chaozi <1243352372@qq.com>
2019-09-10 13:13:12 +0800

    no message

--- maxProfit.go
+++ maxProfit.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(maxProfit([]int{7,1,5,3,6,4}))
+	fmt.Println(maxProfit([]int{1,2,3,4,5,6,9}))
 }
 func maxProfit(prices []int) int {
 
@@ -13,7 +13,11 @@ func maxProfit(prices []int) int {
 		return totalProfit
 	}
 
-	
+	for i:=1 ;i<len(prices); i++{
+		if prices[i] > prices[i - 1] {
+			totalProfit += prices[i] - prices[i - 1];
+		}
+	}
 
 	return totalProfit
 }
